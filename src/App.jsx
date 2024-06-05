8c7f6400d2e8b8e3689f7136d9aec88b04bec150
anasfalah3 <anasfalah3@gmail.com>
2024-06-06 00:04:29 +0100

    defining routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/index.jsx";
 
 function App() {
   return (
     <>
-      <Header />
+    <RouterProvider router={router}/>
+      {/* <Header />
       <Home />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
