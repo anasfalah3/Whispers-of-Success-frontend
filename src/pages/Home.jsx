2aae174cd38633a1adbdc621dbb3f4c7c32f8aca
anasfalah3 <anasfalah3@gmail.com>
2024-07-07 22:57:27 +0100

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
-
+import MusicPage from "../components/podcasts/page";
+// import AlbumArtwork from "../components/podcasts/components/album-artwork";
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Home</h1>
+      {/* <AlbumArtwork /> */}
+      <MusicPage />
+    </div>
+  );
 }
