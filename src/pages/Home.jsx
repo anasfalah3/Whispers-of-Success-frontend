88a0b56c43842c4aa9b5cf62930227384a96f9fb
anasfalah3 <anasfalah3@gmail.com>
2024-07-20 22:09:22 +0100

    home front end v1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
-import MusicPage from "../components/podcasts/page";
+import Hero from "@/components/Hero";
+// import MusicPage from "../components/podcasts/page";
+import CategoriesSlider from "@/components/CategoriesSlider";
+import Podcasts from "@/components/Podcasts";
 // import AlbumArtwork from "../components/podcasts/components/album-artwork";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Hero />
+      <CategoriesSlider/>
+      <div className="text-center m-8">
+        <h1 className="text-3xl font-bold">Featured Podcasts</h1>
+        <p className="text-lg">Listen to our top picks</p>
+      </div>
+      <Podcasts />
+      <div className="text-center m-8">
+        <h1 className="text-3xl font-bold">New Releases</h1>
+      </div>
+      <Podcasts /><Podcasts /><Podcasts />
       {/* <AlbumArtwork /> */}
-      <MusicPage />
+      {/* <MusicPage /> */}
     </div>
   );
 }
