38009fbcc58a28661f045e93d85a158a43ef7081
anasfalah3 <anasfalah3@gmail.com>
2024-07-20 22:07:49 +0100

    Featured Podcasts section

---
+++ src/components/Podcasts.jsx
@@ -0,0 +1,67 @@
+import { Bookmark, Download, Heart, MessageCircle } from "lucide-react";
+import { PodcastsData } from "./podcastsData";
+
+export default function Podcasts() {
+  return (
+      <section>
+            <div className="flex justify-between flex-wrap mb-4">
+            {PodcastsData.map((podcast,index) => (
+                  <>
+                  <div className="mb-5" key={index}>
+                  <div className="border-blue-300 border-2 rounded-2xl relative overflow-hidden p-7  border-transparent hover:border-blue-500 hover:shadow-md hover:shadow-blue-300">
+                      <div className=" relative block h-full ">
+                          <a href="detail-page.html" className="block">
+                              <img src={podcast.cover} className="block w-full h-52 rounded-2xl object-cover" alt="" />
+                          </a>
+                      </div>
+
+                      <div className=" block pt-3 pb-0 px-5 custom-block-info">
+                          <h5 className="mb-2 text-blue-500 text-xl font-bold">
+                              <a href="detail-page.html">
+                                  {podcast.title}
+                              </a>
+                          </h5>
+
+                          <div className="flex mt-2">
+                              <img src={podcast.avatar} className=" rounded-full w-12 h-12 object-cover mr-2" alt=""/>
+                              <p>{podcast.creator}<strong className="block">{podcast.creatorStatus}</strong></p>
+                          </div>
+
+                          <p className=" text-xs opacity-80 my-3">{podcast.desc}</p>
+
+                          <div className="flex justify-between mt-3">
+                              <a href="#"className="flex align-middle justify-center uppercase ml-1 hover:text-blue-500">
+                                    <Download />
+                                  <span  >{podcast.downloads}</span>
+                              </a>
+
+                              <a href="#" className="flex align-middle justify-center uppercase ml-1 hover:text-blue-500" >
+                                    <Heart />
+                                  <span >{podcast.likes}</span>
+                              </a>
+
+                              <a href="#" className="flex align-middle justify-center uppercase ml-1 hover:text-blue-500">
+                                    <MessageCircle />
+                                  <span >{podcast.comments}</span>
+                              </a>
+                          </div>
+                      </div>
+
+                      <div className="absolute top-0 right-0 m-12 flex flex-col ms-auto">
+                          <a href="#" className="bg-black hover:bg-blue-300 text-white rounded-full flex items-center justify-center text-center w-10 h-10 my-2  pb-1 badge ms-auto">
+                              <Heart />
+                          </a>
+
+                          <a href="#" className="bg-black hover:bg-blue-300 text-white rounded-full flex items-center justify-center text-center w-10 h-10 my-2  pb-1 badge ms-auto">
+                              <Bookmark />
+                          </a>
+                      </div>
+                  </div>
+              </div>
+                  </>
+            ))}
+
+            </div>
+  </section>
+  )
+}
---
+++ src/components/podcastsData.js
@@ -0,0 +1,54 @@
+
+import cover1 from "../assets/cover/cover1.jpg";
+import cover2 from "../assets/cover/cover2.jpg";
+import cover3 from "../assets/cover/cover3.jpg";
+import avatar1 from "../assets/avatar/avatar1.jpg";
+import avatar2 from "../assets/avatar/avatar5.jpg";
+import avatar3 from "../assets/avatar/avatar6.jpg";
+
+
+export const PodcastsData = [
+
+  {
+    id: 1,
+    title: "Daily Talk",
+    duration: 50,
+    creator: "Anas Falah",
+    creatorStatus: "Influencer",
+    avatar: avatar1,
+    desc: "Lorem Ipsum dolor sit amet consectetur",
+    cover: cover1,
+    downloads: "300k",
+    likes: "20k",
+    comments: "10k",
+    views: "1M",
+  },
+  {
+    id: 2,
+    title: "Daily Talk",
+    duration: 50,
+    creator: "Anas Falah",
+    creatorStatus: "Vlogger",
+    avatar: avatar2,
+    desc: "Lorem Ipsum dolor sit amet consectetur",
+    cover: cover2,
+    downloads: "30k",
+    likes: "72k",
+    comments: "500",
+    views: "320k",
+  },
+  {
+    id: 3,
+    title: "Vintage Show",
+    duration: 50,
+    creator: "Anas Falah",
+    creatorStatus: "Creator",
+    avatar: avatar3,
+    desc: "Lorem Ipsum dolor sit amet consectetur",
+    cover: cover3,
+    downloads: "500k",
+    likes: "10k",
+    comments: "320",
+    views: "3M",
+  },
+];
