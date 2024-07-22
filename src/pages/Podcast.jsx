dc40b6fcc7fcb317b566fb182c958278779f30e0
anasfalah3 <anasfalah3@gmail.com>
2024-07-22 22:54:58 +0100

    Episodes list (beta)

--- src/pages/Podcast.jsx
+++ src/pages/Podcast.jsx
@@ -1,11 +1,13 @@
 import { useParams } from 'react-router-dom';
 import PodcastDetails from '@/components/PodcastDetails';
+import Episodes from '@/components/Episodes';
 
 const Podcast = () => {
   const { id } = useParams();
   return (
     <div>
         <PodcastDetails id={id}/>
+        <Episodes/>
     </div>
   );
 };
